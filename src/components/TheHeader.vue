6c3c821d794e0a0300a992f0b6cd3f5ea7205762
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-09 17:20:33 +0300

    Get rid of boilerplate files

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="border">
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about">About</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
