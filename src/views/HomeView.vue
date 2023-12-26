f915204ad451d50487af8fe82c4ff12112e75b17
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-26 11:49:06 +0300

    feat: create sidebar comoponent (#16)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import TheFilter from '@/components/TheFilter.vue';
+import TheSidebar from '@/components/TheSidebar.vue';
 
 </script>
 
 <template>
   <TheFilter />
-  <main class="flex min-h-screen justify-center pt-24">
+  <main class="flex min-h-screen">
+    <TheSidebar />
     <h2>Homepage</h2>
   </main>
 </template>
