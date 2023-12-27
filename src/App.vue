afc008891c283caf923fc5d07038bf47a02c4a30
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-27 10:56:05 +0300

    refactor: add TheNavbar component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import TheNavbar from './components/TheNavbar.vue'
 </script>
 
 <template>
+  <TheNavbar />
   <RouterView />
 </template>
