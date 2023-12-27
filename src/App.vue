d261889974ac15a5eb208ee27f0d578ad2bc5bcc
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-27 17:59:08 +0300

    refactor: turn off eslint rule that forces multi-word component names

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import TheNavbar from './components/TheNavbar.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <TheNavbar />
+  <Navbar />
   <RouterView />
 </template>
