dd87f48607ee84be249d04359490d02d807de0d6
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-30 21:44:17 +0300

    fetch data from a local json data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Sidebar from '@/components/Sidebar.vue'
 import Filter from '@/components/Filter.vue'
+import { jobs } from '@/data/jobs.ts'
 </script>
 
 <template>
   <Filter />
-  <div class="antialiased bg-gray-50 dark:bg-gray-900 flex overflow-hidden">
+  <div class="antialiased bg-gray-50 dark:bg-gray-900 flex max-w-screen">
     <Sidebar />
     <main class="p-4 h-auto border w-full">
       <div class="flex items-center gap-2">
@@ -13,15 +14,19 @@ import Filter from '@/components/Filter.vue'
         <div class="badge badge-outline">128</div>
       </div>
 
-      <div class="flex mt-4 gap-4">
-        <div class="card w-96 shadow-md border bg-white glass p-1">
-          <div class="card-body p-4 bg-slate-200 rounded-xl shadow-sm">
+      <div class="flex mt-4 gap-4 flex-wrap">
+        <div
+          v-for="job in jobs.featured"
+          :key="job.id"
+          class="card w-64 shadow-md border bg-white glass p-1"
+        >
+          <div class="card-body p-4 rounded-xl shadow-sm" :class="'bg-' + job.color + '-200'">
             <div class="flex">
               <div class="badge bg-white py-3">Feb 24, 2023</div>
             </div>
             <h2 class="card-title">Card title!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="flex gap-1">
+            <div class="flex gap-1 flex-wrap">
               <div class="badge badge-outline py-3 px-2 border-stone-400">default</div>
               <div class="badge badge-outline py-3 px-2 border-stone-400">primary</div>
               <div class="badge badge-outline py-3 px-2 border-stone-400">secondary</div>
