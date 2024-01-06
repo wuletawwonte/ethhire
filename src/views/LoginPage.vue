3da6ce916e95ef68f775d10caa44b5024f59b515
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-01-06 14:28:21 +0300

    position login box properly

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,7 +1,39 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex-grow">
-    <h2>Login</h2>
+  <div class="flex-grow bg-base-200">
+    <div class="relative flex flex-col mt-16 mx-4 justify-center overflow-hidden">
+      <div class="w-full p-6 m-auto bg-base-100 rounded-md shadow-md lg:max-w-lg">
+        <h1 class="text-2xl text-center">Login</h1>
+        <form class="space-y-4">
+          <div>
+            <label class="label">
+              <span class="text-base label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Email Address"
+              class="w-full input input-bordered input-primary"
+            />
+          </div>
+          <div>
+            <label class="label">
+              <span class="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              class="w-full input input-bordered input-primary"
+            />
+          </div>
+          <a href="#" class="text-xs text-gray-600 hover:underline hover:text-blue-600"
+            >Forget Password?</a
+          >
+          <div>
+            <button class="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
