6d4bada996e722cdd607ded1751bd7a4e5277e33
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-01-06 16:17:03 +0300

    make login button block

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -3,8 +3,15 @@
 <template>
   <div class="flex-grow bg-base-200">
     <div class="relative flex flex-col mt-16 mx-4 justify-center overflow-hidden">
-      <div class="w-full p-6 m-auto bg-base-100 rounded-md shadow-md lg:max-w-lg">
-        <h1 class="text-2xl text-center">Login</h1>
+      <div
+        class="w-full border-t-4 border-gray-600 p-6 m-auto bg-base-100 rounded-md shadow-md lg:max-w-lg"
+      >
+        <h1 class="text-2xl text-center">Ethhire account</h1>
+
+        <p class="text-center text-base text-gray-600">
+          Login to your account to unlock more jobs.
+        </p>
+
         <form class="space-y-4">
           <div>
             <label class="label">
@@ -13,7 +20,8 @@
             <input
               type="text"
               placeholder="Email Address"
-              class="w-full input input-bordered input-primary"
+              class="w-full input input-bordered focus:input-primary"
+              focus
             />
           </div>
           <div>
@@ -23,14 +31,14 @@
             <input
               type="password"
               placeholder="Enter Password"
-              class="w-full input input-bordered input-primary"
+              class="w-full input input-bordered focus:input-primary"
             />
           </div>
           <a href="#" class="text-xs text-gray-600 hover:underline hover:text-blue-600"
             >Forget Password?</a
           >
           <div>
-            <button class="btn btn-primary">Login</button>
+            <button class="btn btn-primary btn-block">Login</button>
           </div>
         </form>
       </div>
